e7dec23adc54a3143c7b969732276d3442e6b19d
Connor Taylor <connor@cogolabs.com>
2019-03-28 14:23:04 -0400

    load issues

--- weekly-updates.js
+++ weekly-updates.js
@@ -1,5 +1,15 @@
-const getIssues = () => {
+const fs = require('fs');
 
+const getIssues = () => {
+  // TODO: query the github API to get these issues
+  var issues = fs.readdirSync('./previous_issues');
+  var issueMarkdowns = [];
+  for (var i = 0; i < issues.length; i++) {
+    var issueMarkdown = fs.readFileSync('./previous_issues/' + issues[i], { encoding: 'utf8' });
+    issueMarkdowns.push(issueMarkdown);
+  }
+
+  return issueMarkdowns;
 };
 
 const getSection = (issue) => {
@@ -7,7 +17,7 @@ const getSection = (issue) => {
 };
 
 const closeIssues = (issues) => {
-
+  // TODO: implement this lol
 };
 
 const createIssues = (issues) => {
@@ -15,7 +25,8 @@ const createIssues = (issues) => {
 };
 
 const main = () => {
-
+  let issues = getIssues();
+  console.log(issues);
 };
 
 main();
