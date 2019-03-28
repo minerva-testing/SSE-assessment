dad71afbaf396f56b74a8aa9a0efad5491c4814b
Connor Taylor <connor@cogolabs.com>
2019-03-28 15:14:37 -0400

    create new issues

--- weekly-updates.js
+++ weekly-updates.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var moment = require('moment');
 
 const getIssues = () => {
   // TODO: query the github API to get these issues
@@ -6,27 +7,43 @@ const getIssues = () => {
   var issueMarkdowns = [];
   for (var i = 0; i < issues.length; i++) {
     var issueMarkdown = fs.readFileSync('./previous_issues/' + issues[i], { encoding: 'utf8' });
-    issueMarkdowns.push(issueMarkdown);
+    issueMarkdowns.push({ user: issues[i].replace('.md', ''), update: issueMarkdown });
   }
 
   return issueMarkdowns;
 };
 
-const getSection = (issue) => issue.split('## Tasks for this week')[1];
+const getSection = (issue) => issue.update.split('## Tasks for this week')[1];
 
 const closeIssues = (issues) => {
   // TODO: implement this lol
 };
 
 const createIssues = (issues) => {
+  for (var i = 0; i < issues.length; i++) {
+    let issue = issues[i];
+    let previousUpdate= getSection(issue);
+    let today = `${moment().month()}/${moment().day()}`;
+    let endOfWeek = moment().add(5, 'days');
+    let endOfWeekString = `${endOfWeek.month()}/${endOfWeek.day()}`;
+  const newUpdate = `
+# Update for @${issue.user} for ${today}-${endOfWeekString}
 
+## Tasks from last week
+
+${previousUpdate}
+
+# Tasks for this week
+`;
+
+  fs.writeFileSync(`./new_issues/${issue.user}.md`, newUpdate);
+}
 };
 
 const main = () => {
   let issues = getIssues();
-  let sections = issues.map(getSection);
-
-  console.log(sections);
+  // closeIssues(issues);
+  createIssues(issues);
 };
 
 main();
